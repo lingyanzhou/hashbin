d930d80ff08f4bb88b6c60ff0471a3ecf173eeaf
zhoulingyan <lingyan_zhou_0228@163.com>
2019-06-11 17:50:12 +0800

    tmp

--- src/main/java/lzhou/learning/hash/hashbin/controller/DigitalSignatureAjaxController.java
+++ src/main/java/lzhou/learning/hash/hashbin/controller/DigitalSignatureAjaxController.java
@@ -3,8 +3,15 @@ package lzhou.learning.hash.hashbin.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Signature;
+
 /**
- * @Description:
+ * @Description: 数字签名 (Digital Signature)
+ * - 定义
+ *   - 一个
+ *   -
+ *   -
+ *
  * @author: lingy
  * @Date: 2019-06-11 14:40:17
  * @param: null
@@ -15,3 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DigitalSignatureAjaxController {
 }
+
+// AES/DEA   RSA  RSA2  DSA  .pem .pk8  jks  openssl
+// 加解密(AES,RSA)和签名(SHA1WithRSA,SHA256WithRSA)
---
+++ src/main/java/lzhou/learning/hash/hashbin/controller/SymmetricEncryption.java
@@ -0,0 +1,18 @@
+package lzhou.learning.hash.hashbin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ *
+ * [第二篇：对称加密及AES加密算法](https://www.jianshu.com/p/3840b344b27c?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation)
+ * @author: lingy
+ * @Date: 2019-06-11 17:48:44
+ * @param: null
+ * @return:
+ */
+@RestController
+@RequestMapping()
+public class SymmetricEncryption {
+}
